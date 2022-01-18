6d673559a9378cbf16e5e1608e5b2437a03ed9c8
shivaninkulkarni23 <65147624+shivaninkulkarni23@users.noreply.github.com>
2022-01-18 21:41:50 +0530

    Add files via upload

---
+++ Shivani Kulkarni printvariations.py
@@ -0,0 +1,10 @@
+print("Hello")
+###
+name='shivani'
+age=23
+marks=0;
+print("My name is %s and age is %s"%(name,age))
+print("My name is {} and age is {}".format(name,age,marks))
+print (f"My name is {name} and age is{age}")
+print(f"{2+2}")
+
