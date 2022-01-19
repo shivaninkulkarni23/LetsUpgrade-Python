7aaff4be280d59cc97b636adea057db1f52c698a
shivaninkulkarni23 <65147624+shivaninkulkarni23@users.noreply.github.com>
2022-01-19 21:10:26 +0530

    Add files via upload
    Day 3

---
+++ Shivani Kulkarni-Day 3.py
@@ -0,0 +1,7 @@
+name=input(f"Enter name :")
+age=int(input(f"Enter age :"))
+address=input(f"Enter address:")
+
+items=[name,age,address]
+for i in items:
+    print (f"name is :{name} age is :{age} address is :{address}")
